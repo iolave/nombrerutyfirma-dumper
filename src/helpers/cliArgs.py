4325d3094a01b27e6e4ea6b73f0f52434bf27e29
Ignacio Olave <olave.ia@icloud.com>
2022-09-12 09:16:07 -0300

    Added explanation/documentacion to cliArgs

--- src/helpers/cliArgs.py
+++ src/helpers/cliArgs.py
@@ -1,6 +1,11 @@
-# Returns a python object with 
-# all the given cli arguments
-# in a key value maner
+# Input: An array of cli options
+# Output: Python object with all 
+# the given cli arguments in a 
+# key value style 
+# 
+# example:
+#   For input: ['--first']
+#   Returns: [{'key': 'first', 'value':None}]
 def cliArgumentsParser(arr):
     # The first argument argument is the bin file
     # so we pop it as we're not gonna parse it
@@ -9,10 +14,18 @@ def cliArgumentsParser(arr):
     # Create an empty array to store all key value
     # pairs
     res = []
+
+
     for arg in arr:
+        # If equals char is found, splits the arg
+        # and obtain a key and a value pair.
+        # Otherwise assumes None as key's value
+        # which means it's default value
         if arg.find('=') != -1:
             key, value = arg.split('=')
         else:
             key, value = arg, None
         res.append({"key": key, "value": value})
+
+    # Return all the key value pairs
     return(res)

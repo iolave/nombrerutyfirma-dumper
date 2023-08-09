208154d32c77756b09427952b9ab4c54c79b1642
Ignacio Olave <olave.ia@protonmail.com>
2023-08-09 18:58:49 -0400

    added console output method

--- src/business-logic/commander/index.ts
+++ src/business-logic/commander/index.ts
@@ -6,22 +6,39 @@ import searchByRut from "../../business-logic/elrutificador/search-by-rut";
 import { formatRut } from "../../util/rut";
 
 export default async function commanderAction(opts: Options) {
-    if (opts.source === "elrutificador") {
-        if (opts.outFile) {
-            const outFilePath = path.resolve(opts.outPath ?? "");
-            const writeStream = fs.createWriteStream(outFilePath);
-
-            const formattedRut = formatRut(opts.rut);
-            if (!formattedRut) {
-                log.error(`given rut ${opts.rut} is not a valid`)
-                process.exit(1);
-            };
-
-            const data = await searchByRut(formattedRut);
-            log.debug(outFilePath);
-            writeStream.write(Buffer.from(JSON.stringify(data).concat(EOL)));
-            writeStream.close();
+    if (opts.destination === "console") {
+        const formattedRut = formatRut(opts.rut);
+        if (!formattedRut) {
+            log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+            process.exit(1);
         }
+        
+        if (opts.source === "elrutificador") {
+            const data = await searchByRut(formattedRut);
+            console.table(data);
+            process.exit(0);
+        }
+        process.exit(0);
+    }
+    else if (opts.destination === "local-file") {
+        const outFilePath = path.resolve(opts.outPath ?? "");
+        const writeStream = fs.createWriteStream(outFilePath);
+
+        const formattedRut = formatRut(opts.rut);
+        if (!formattedRut) {
+            log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+            process.exit(1);
+        }
+
+        if (opts.source === "elrutificador") {
+            const data = await searchByRut(formattedRut);
+            log.info(`${opts.source}: data found for rut ${opts.rut}`);
+            writeStream.write(Buffer.from(JSON.stringify(data).concat(EOL)));
+            log.info(`${opts.source}: wrote found data for rut ${opts.rut} to ${opts.outPath}`);
+            writeStream.close();
+            process.exit(0);
+        }
+        process.exit(0);
     }
 }
 
@@ -30,8 +47,12 @@ type Options = {
     outFile: boolean;
     outPath?: string;
     rut: string;
+    destination: Destination;
 }
 
 
 export const informationSources = ["elrutificador"] as const;
 export type InformationSource = typeof informationSources[number];
+
+export const destinations = ["console", "local-file"] as const;
+export type Destination = typeof destinations[number];
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,54 @@
 import { Option, program } from "commander";
-import commanderAction, { InformationSource, informationSources } from "./business-logic/commander";
+import commanderAction, { Destination, InformationSource, destinations, informationSources } from "./business-logic/commander";
 
 const sourceOption = new Option("--source <source>", "information source");
 sourceOption.choices(informationSources);
 sourceOption.makeOptionMandatory(true);
 
-const outFileOption = new Option('--out-file <path>', "write output to a local file");
-outFileOption.makeOptionMandatory(true);
+const outputOption = new Option('--output <dest>', "write to the desired output");
+outputOption.choices(destinations);
+outputOption.makeOptionMandatory(true);
+
+const outPathOption = new Option('--out-path <path>', "out path");
+// outPathOption.makeOptionMandatory(true).conflicts("output=console");
 
 const rutOption = new Option('--rut <rut>', "search a single rut");
 rutOption.makeOptionMandatory(true);
 
 type ProgramOptions = {
     source: InformationSource;
-    outFile: string;
+    output: Destination;
     rut: string;
+    outPath?: string;
 }
 
 program.name("nryf-dumper");
 program.showHelpAfterError();
 program.addOption(sourceOption);
-program.addOption(outFileOption);
+program.addOption(outputOption);
+program.addOption(outPathOption);
 program.addOption(rutOption);
 program.action(async (args: ProgramOptions) => {
+
+    if (args.output === "console") {
+        if (args.outPath) {
+            program.error(`error: option '--out-path <path>' can not be used with '--output ${args.output}'`)
+        }
+    }
+
+    if (args.output === "local-file") {
+        if (!args.outPath) {
+            program.error(`error: required option '--out-path <path>' not specified`)
+        }
+    }
+    
     commanderAction({
+        destination: args.output,
         source: args.source,
         outFile: true,
-        outPath: args.outFile,
+        outPath: args.outPath,
         rut: args.rut,
     });
 })
-program.parse();
 
+program.parse();

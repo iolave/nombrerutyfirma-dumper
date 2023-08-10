b426d460ce59bd43b3b912620e8614fadd3731f4
Ignacio Olave <olave.ia@protonmail.com>
2023-08-09 20:25:30 -0400

    added elrutificador:local-file support for --from-rut and --to-rut

--- src/business-logic/commander/index.ts
+++ src/business-logic/commander/index.ts
@@ -3,42 +3,73 @@ import fs from "fs";
 import { EOL } from "os";
 import log from "../../config/logger";
 import searchByRut from "../../business-logic/elrutificador/search-by-rut";
-import { formatRut } from "../../util/rut";
+import { formatRut, calculateDv } from "../../util/rut";
 
 export default async function commanderAction(opts: Options) {
     if (opts.destination === "console") {
-        const formattedRut = formatRut(opts.rut);
-        if (!formattedRut) {
-            log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+        if (typeof opts.rut === "string") {
+            const formattedRut = formatRut(opts.rut);
+            if (!formattedRut) {
+                log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+                process.exit(1);
+            }
+            
+            if (opts.source === "elrutificador") {
+                const data = await searchByRut(formattedRut);
+                console.table(data);
+                process.exit(1);
+            }
             process.exit(1);
         }
-        
-        if (opts.source === "elrutificador") {
-            const data = await searchByRut(formattedRut);
-            console.table(data);
-            process.exit(0);
-        }
-        process.exit(0);
+        process.exit(1);
     }
     else if (opts.destination === "local-file") {
         const outFilePath = path.resolve(opts.outPath ?? "");
         const writeStream = fs.createWriteStream(outFilePath);
+        if (typeof opts.rut === "string") {
+            const formattedRut = formatRut(opts.rut);
+            if (!formattedRut) {
+                log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+                process.exit(1);
+            }
 
-        const formattedRut = formatRut(opts.rut);
-        if (!formattedRut) {
-            log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+            if (opts.source === "elrutificador") {
+                const data = await searchByRut(formattedRut);
+                log.info(`${opts.source}: data found for rut ${opts.rut}`);
+                writeStream.write(Buffer.from(JSON.stringify(data).concat(EOL)));
+                log.info(`${opts.source}: wrote found data for rut ${opts.rut} to ${opts.outPath}`);
+                writeStream.close();
+                process.exit(0);
+            }
+            process.exit(1);
+        } else {
+            
+            if (opts.source === "elrutificador") {
+                for (var i = opts.rut.from;i <= opts.rut.to;i++) {
+                    const rut = `${i}-${calculateDv(i)}`;
+                    const formattedRut = formatRut(rut);
+                    if (!formattedRut) {
+                        log.error(`${opts.source}: given rut ${opts.rut} is not a valid`)
+                        process.exit(1);
+                    }
+                    const data = await searchByRut(formattedRut)
+                        .catch(() => undefined)
+                    ;
+
+                    if (data) {
+                        log.info(`${opts.source}: data found for rut ${rut}`);
+                        writeStream.write(Buffer.from(JSON.stringify(data).concat(EOL)));
+                        log.info(`${opts.source}: wrote found data for rut ${rut} to ${opts.outPath}`);
+                    } else {
+                        log.info(`${opts.source}: data not found for rut ${rut}, skipping`);
+                    }
+                }
+                writeStream.close();
+                process.exit(0);
+            }
             process.exit(1);
         }
-
-        if (opts.source === "elrutificador") {
-            const data = await searchByRut(formattedRut);
-            log.info(`${opts.source}: data found for rut ${opts.rut}`);
-            writeStream.write(Buffer.from(JSON.stringify(data).concat(EOL)));
-            log.info(`${opts.source}: wrote found data for rut ${opts.rut} to ${opts.outPath}`);
-            writeStream.close();
-            process.exit(0);
-        }
-        process.exit(0);
+        process.exit(1);
     }
 }
 
@@ -46,7 +77,7 @@ type Options = {
     source: InformationSource;
     outFile: boolean;
     outPath?: string;
-    rut: string;
+    rut: string | { from: number, to: number };
     destination: Destination;
 }
 
--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,41 @@ outputOption.choices(destinations);
 outputOption.makeOptionMandatory(true);
 
 const outPathOption = new Option('--out-path <path>', "out path");
-// outPathOption.makeOptionMandatory(true).conflicts("output=console");
+outPathOption.makeOptionMandatory(false);
 
 const rutOption = new Option('--rut <rut>', "search a single rut");
-rutOption.makeOptionMandatory(true);
+rutOption.makeOptionMandatory(false);
+rutOption.conflicts("fromRut");
+rutOption.conflicts("toRut");
+
+const fromRutOption = new Option('--from-rut <rut without dv>', "search a bunch of ruts starting from this rut. eg: 1");
+fromRutOption.makeOptionMandatory(false);
+fromRutOption.conflicts("rut");
+
+const toRutOption = new Option('--to-rut <rut without dv>', "search a bunch of ruts ending with this rut. eg: 1000000");
+toRutOption.makeOptionMandatory(false);
+toRutOption.conflicts("rut");
 
 type ProgramOptions = {
     source: InformationSource;
     output: Destination;
-    rut: string;
+    rut?: string;
     outPath?: string;
+    fromRut?: string;
+    toRut?: string;
 }
 
 program.name("nryf-dumper");
 program.showHelpAfterError();
+
 program.addOption(sourceOption);
 program.addOption(outputOption);
 program.addOption(outPathOption);
 program.addOption(rutOption);
-program.action(async (args: ProgramOptions) => {
+program.addOption(fromRutOption);
+program.addOption(toRutOption);
 
+program.action(async (args: ProgramOptions) => {
     if (args.output === "console") {
         if (args.outPath) {
             program.error(`error: option '--out-path <path>' can not be used with '--output ${args.output}'`)
@@ -41,13 +56,27 @@ program.action(async (args: ProgramOptions) => {
             program.error(`error: required option '--out-path <path>' not specified`)
         }
     }
+
+    if (!args.rut) {
+        if (!args.fromRut) {
+            program.error(`error: required option '--from-rut <rut without dv>' not specified`)
+        }
+        if (!args.toRut) {
+            program.error(`error: required option '--to-rut <rut without dv>' not specified`)
+        }
+    }
+
+    const rut = args.rut ?? {
+        from: parseInt(args.fromRut??""),
+        to: parseInt(args.toRut??"")
+    }
     
     commanderAction({
         destination: args.output,
         source: args.source,
         outFile: true,
         outPath: args.outPath,
-        rut: args.rut,
+        rut,
     });
 })
 

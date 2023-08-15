01a6dd3ad687f731bd170c59b610db4904cadc3e
Ignacio Olave <olave.ia@icloud.com>
2023-08-14 21:10:52 -0400

    added rut options input validation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Option, program } from "commander";
 import commanderAction, { Destination, InformationSource, destinations, informationSources } from "./business-logic/commander";
+import { isNumber } from "./util/string";
 
 const sourceOption = new Option("--source <source>", "information source");
 sourceOption.choices(informationSources);
@@ -12,23 +13,38 @@ outputOption.makeOptionMandatory(true);
 const outPathOption = new Option('--out-path <path>', "out path");
 outPathOption.makeOptionMandatory(false);
 
-const rutOption = new Option('--rut <rut>', "search a single rut");
+const rutOptionFlags = "--rut <rut without dv>";
+const rutOption = new Option(rutOptionFlags, "search a single rut");
 rutOption.makeOptionMandatory(false);
 rutOption.conflicts("fromRut");
 rutOption.conflicts("toRut");
+rutOption.argParser((value) => {
+    if (isNumber(value)) return parseInt(value);
+    program.error(`${rutOptionFlags} value should be a number`);
+});
 
-const fromRutOption = new Option('--from-rut <rut without dv>', "search a bunch of ruts starting from this rut. eg: 1");
+const fromRutOptionFlags = "--from-rut <rut without dv>";
+const fromRutOption = new Option(fromRutOptionFlags, "search a bunch of ruts starting from this rut. eg: 1");
 fromRutOption.makeOptionMandatory(false);
 fromRutOption.conflicts("rut");
+fromRutOption.argParser((value) => {
+    if (isNumber(value)) return parseInt(value);
+    program.error(`${fromRutOptionFlags} value should be a number`);
+});
 
-const toRutOption = new Option('--to-rut <rut without dv>', "search a bunch of ruts ending with this rut. eg: 1000000");
+const toRutOptionFlags = "--to-rut <rut without dv>";
+const toRutOption = new Option(toRutOptionFlags, "search a bunch of ruts ending with this rut. eg: 1000000");
 toRutOption.makeOptionMandatory(false);
 toRutOption.conflicts("rut");
+toRutOption.argParser((value) => {
+    if (isNumber(value)) return parseInt(value);
+    program.error(`${fromRutOptionFlags} value should be a number`);
+});
 
 type ProgramOptions = {
     source: InformationSource;
     output: Destination;
-    rut?: string;
+    rut?: number;
     outPath?: string;
     fromRut?: string;
     toRut?: string;

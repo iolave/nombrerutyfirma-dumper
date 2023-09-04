129765daca4abe91f8aadffc4b5125b31c56daf5
Ignacio Olave <olave.ia@icloud.com>
2023-09-04 17:15:23 -0300

    moved to new elrutificador jwt token generation

--- src/information-sources/elrutificador/search-by-rut.ts
+++ src/information-sources/elrutificador/search-by-rut.ts
@@ -1,16 +1,18 @@
-import { unixTimestamp } from "../../util/date";
+// import { unixTimestamp } from "../../util/date";
 import log from "../../config/logger";
 
 export default async function elrutificadorByRut(rut: string, maxRetries?: number): Promise<ElRutificadorResponse> {
     try {
         log.debug(`elrutificador: querying person by rut ${rut}`);
         const token = await retrieveToken(rut);
-        log.debug(`elrutificador: retrieved token for rut ${rut}`);
-        const html = await retrieveHtml(token);
-        log.debug(`elrutificador: retrieved html for ${rut}`);
-        const data = extractDataFromHtml(html);
-        log.debug(`elrutificador: scrapped html for ${rut}`);
-        return data;
+        log.debug(`elrutificador: retrieved token for rut ${rut}: ${token}`);
+        // const html = await retrieveHtml(token);
+        // console.log(html);
+        process.exit(0);
+        // log.debug(`elrutificador: retrieved html for ${rut}`);
+        // const data = extractDataFromHtml(html);
+        // log.debug(`elrutificador: scrapped html for ${rut}`);
+        // return data;
     } catch (error: unknown) {
         return handleRetry(rut, error, maxRetries)
     }
@@ -45,97 +47,115 @@ async function handleRetry(rut: string, error: unknown, retriesLeft?: number): P
     throw error;
 }
 
-function formatBirthdate(str: string): string {
-    if (str === "") return str;
-    if (!str.match(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}/)) return str;
-    return `${str.slice(6, 10)}-${str.slice(3, 5)}-${str.slice(0, 2)}`;
-}
+// function formatBirthdate(str: string): string {
+//     if (str === "") return str;
+//     if (!str.match(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}/)) return str;
+//     return `${str.slice(6, 10)}-${str.slice(3, 5)}-${str.slice(0, 2)}`;
+// }
 
-function extractDataFromHtml(html: string): ElRutificadorResponse {
-    const withoutLineBreaks = html.replace(/[\r\n]/gm, '');
-    const tableRegexp = /<table.*>.*<\/table>/;
-    const htmlTable = withoutLineBreaks.match(tableRegexp)?.[0];
+// function extractDataFromHtml(html: string): ElRutificadorResponse {
+//     const withoutLineBreaks = html.replace(/[\r\n]/gm, '');
+//     const tableRegexp = /<table.*>.*<\/table>/;
+//     const htmlTable = withoutLineBreaks.match(tableRegexp)?.[0];
 
-    if (!htmlTable) throw new Error("elrutificador_error: no table found in html");
+//     if (!htmlTable) throw new Error("elrutificador_error: no table found in html");
 
-    const itemsRegexp = /(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)/
-    const itemRegexp = /<td.*>.*<\/td>\s*<td.*>(.*)<\/td>/
+//     const itemsRegexp = /(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)(<tr>.*<\/tr>\s*)/
+//     const itemRegexp = /<td.*>.*<\/td>\s*<td.*>(.*)<\/td>/
 
-    const items = htmlTable.match(itemsRegexp)?.map(e => e.match(itemRegexp)?.[1]);
-    return {
-        id: items?.at(2) ?? "",
-        name: items?.at(3) ?? "",
-        address: items?.at(5) ?? "",
-        gender: items?.at(6)?.toUpperCase() === "MASCULINO" ? "MALE": "FEMALE",
-        city: items?.at(7) ?? "",
-        birthdate: formatBirthdate(items?.at(8) ?? ""),
-        timestamp: unixTimestamp(),
-        source: "elrutificador.com",
-    };
-}
+//     const items = htmlTable.match(itemsRegexp)?.map(e => e.match(itemRegexp)?.[1]);
+//     return {
+//         id: items?.at(2) ?? "",
+//         name: items?.at(3) ?? "",
+//         address: items?.at(5) ?? "",
+//         gender: items?.at(6)?.toUpperCase() === "MASCULINO" ? "MALE": "FEMALE",
+//         city: items?.at(7) ?? "",
+//         birthdate: formatBirthdate(items?.at(8) ?? ""),
+//         timestamp: unixTimestamp(),
+//         source: "elrutificador.com",
+//     };
+// }
 
-async function retrieveHtml(token: string): Promise<string> {
-    const url = new URL("https://elrutificador.com");
-    url.pathname = "/resultados";
+// async function retrieveHtml(token: string): Promise<string> {
+//     const url = new URL("https://elrutificador.com");
+//     url.pathname = "/resultados/";
 
-    const headers = new Headers();
-    headers.append("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-    headers.append("Accept-Encoding", "gzip, deflate, br");
-    headers.append("Accept-Language", "en-US,en;q=0.9");
-    headers.append("Connection", "keep-alive");
-    headers.append("Host", "elrutificador.com");
-    headers.append("Origin", "https://elrutificador.com");
-    headers.append("Referer", "https://elrutificador.com/");
-    headers.append("Sec-Fetch-Site", "same-origin");
-    headers.append("Sec-Fetch-Mode", "Navigate");
-    headers.append("Sec-Fetch-Dest", "document");
-    headers.append("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15");
-    headers.append("Content-Type", "application/x-www-form-urlencoded");
+//     const headers = new Headers();
+//     headers.append("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+//     headers.append("Accept-Encoding", "gzip, deflate, br");
+//     headers.append("Accept-Language", "en-US,en;q=0.9");
+//     headers.append("Connection", "keep-alive");
+//     headers.append("Host", "elrutificador.com");
+//     headers.append("Origin", "https://elrutificador.com");
+//     headers.append("Referer", "https://elrutificador.com/");
+//     headers.append("Sec-Fetch-Site", "same-origin");
+//     headers.append("Sec-Fetch-Mode", "Navigate");
+//     headers.append("Sec-Fetch-Dest", "document");
+//     headers.append("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15");
+//     headers.append("Content-Type", "application/x-www-form-urlencoded");
 
-    const data = new URLSearchParams();
-    data.append("t", token);
-    data.append("MIME Type", "application/x-www-form-urlencoded");
-
-    const requestInit: RequestInit = {
-        method: "POST",
-        headers,
-        body: data.toString(),
-    }
-
-    const res = await fetch(url, requestInit).then(res => res.text());
+//     const requestInit: RequestInit = {
+//         method: "GET",
+//         headers,
+//     }
     
-    return res;
-}
+//     const res = await fetch(url, requestInit).then(res => res.text());
+    
+//     return res;
+// }
 
 async function retrieveToken(rut: string): Promise<string> {
+    console.log(rut);
     const url = new URL("https://elrutificador.com");
-    url.pathname = "/ctk/t";
+    url.pathname = "/";
 
     const headers = new Headers();
-    headers.append("Accept", "application/json");
+    headers.append("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8");
     headers.append("Accept-Encoding", "gzip, deflate, br");
-    headers.append("Accept-Language", "en-US,en;q=0.9");
-    headers.append("Connection", "keep-alive");
-    headers.append("Content-Type", "application/json");
-    headers.append("Host", "elrutificador.com");
+    headers.append("Accept-Language", "en-US,en;q=0.8");
+    headers.append("Cache-Control", "no-cache");
+    headers.append("Cookie", "cf_clearance=YgFoRBZDMkr0lRWLuJKgozrJTw_9Xh9.P3kazNuVpbo-1693857837-0-1-d7193ef.2bb64171.7d8678ed-0.2.1693857837");
     headers.append("Origin", "https://elrutificador.com");
+    headers.append("Pragma", "no-cache");
     headers.append("Referer", "https://elrutificador.com/");
+    headers.append("Sec-Ch-Ua", '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"');
+    headers.append("Sec-Ch-Ua-Mobile", "?0");
+    headers.append("Sec-Ch-Ua-Platform", '"macOS"');
     headers.append("Sec-Fetch-Site", "same-origin");
-    headers.append("Sec-Fetch-Mode", "cors");
-    headers.append("Sec-Fetch-Dest", "empty");
+    headers.append("Sec-Fetch-Mode", "navigate");
+    headers.append("Sec-Fetch-Dest", "document");
+    headers.append("Sec-Fetch-User", "?1");
+    headers.append("Sec-Gpc", "1");
+    headers.append("Upgrade-Insecure-Requests", "1");
     headers.append("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15");
+
+    const formData = new FormData();
+    formData.set("term", "roberto");
+    formData.set("opt", "name");
 
     const requestInit: RequestInit = {
         method: "POST",
         headers,
-        body: JSON.stringify({
-            trm: rut,
-            method: "rut"
-        })
+        body: formData,
     }
 
-    const res = await fetch(url, requestInit).then(res => res.text());
-    if (!res.match(/^[a-zA-Z0-9]+$/)) throw new Error(res);
+    const jwt = await fetch(url, requestInit)
+        .then(async res => { 
+            if ((await res.text()).includes("Access denied")) throw new Error("Elrutificador cf_clearance cookie header expired");
+            return res
+        })
+        .then(res => res.headers.get("Set-Cookie"))
+        .then(setCookie => {
+            if (!setCookie) throw new Error("No set-cookie header in elrutificador response");
+            return setCookie;
+        })
+        .then(setCookie => setCookie.split(";"))
+        .then(cookies => cookies.filter(c => c.includes("jwt=")).at(0))
+        .then(cookie => {
+            if (!cookie) throw new Error("No jwt within set-cookie header in elrutificador response");
+            return cookie.slice(4)
+        })
+    ;
 
-    return res;
+    return jwt;
 }

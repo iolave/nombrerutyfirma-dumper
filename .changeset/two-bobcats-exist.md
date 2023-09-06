66a23e3484f9f126239c40620513d99dd6fc57a8
Ignacio Olave <olave.ia@icloud.com>
2023-09-06 13:07:00 -0300

    added scoped NRYFError error codes

---
+++ .changeset/two-bobcats-exist.md
@@ -0,0 +1,5 @@
+---
+"nombrerutyfirma-dumper": patch
+---
+
+Added scoped NRYFError error codes to have more control over errors
--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,9 +1,19 @@
+const errorCodes = [
+    "cf_ip_banned",
+    "fetch_error",
+    "data_not_found",
+    "no_jwt",
+    "todo_handle_error",
+] as const;
+
+type ErrorCode = typeof errorCodes[number];
+
 export class NRYFError extends Error {
     name: string;
-    code: string;
+    code: ErrorCode;
     message: string;
 
-    constructor(name: string, code: string, message: string) {
+    constructor(name: string, code: ErrorCode, message: string) {
         super();
         this.name = name;
         this.code = code;
